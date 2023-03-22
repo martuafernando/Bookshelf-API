2a3c3f568a79b538c286ac924ee8b59cc2fafb9a
martuafernando <fernandosibarani45@gmail.com>
2023-03-22 16:13:25 +0700

    add functionality each enspoint

--- src/endpoint/books.js
+++ src/endpoint/books.js
@@ -1,7 +1,7 @@
 const { nanoid } = require('nanoid')
 const storedBooks = require('../storage/storedBooks')
 
-module.exports = function storeBooks (request, h) {
+function storeBooks (request, h) {
   const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload
 
   if (!name) {
@@ -35,7 +35,6 @@ module.exports = function storeBooks (request, h) {
   storedBooks.push(book)
 
   const isSuccess = storedBooks.filter((book) => book.id === storedBooks.id)
-
   if (isSuccess) {
     return h
       .response({
@@ -52,3 +51,133 @@ module.exports = function storeBooks (request, h) {
     .response({ status: 'fail', message: 'Gagal menambahkan buku' })
     .code(500)
 }
+
+function getAllBooks (request, h) {
+  const books = []
+  const { reading, finished, name } = request.query
+  let allBooks = storedBooks
+
+  if (reading === '1') allBooks = storedBooks.filter((it) => it.reading === true)
+  if (reading === '0') allBooks = storedBooks.filter((it) => it.reading === false)
+
+  if (finished === '1') allBooks = storedBooks.filter((it) => it.finished === true)
+  if (finished === '0') allBooks = storedBooks.filter((it) => it.finished === false)
+
+  if (name) allBooks = storedBooks.filter((it) => it.name.toLowerCase().includes(name.toLowerCase()))
+
+  allBooks.forEach((book) => {
+    books.push({
+      id: book.id,
+      name: book.name,
+      publisher: book.publisher
+    })
+  })
+
+  return h
+    .response({
+      status: 'success',
+      data: {
+        books
+      }
+    })
+    .code(200)
+}
+
+function getDetailBook (request, h) {
+  const { bookId } = request.params
+  const book = storedBooks.filter((it) => it.id === bookId)[0]
+
+  if (!book) {
+    return h
+      .response({ status: 'fail', message: 'Buku tidak ditemukan' })
+      .code(404)
+  }
+
+  return h
+    .response({
+      status: 'success',
+      data: {
+        book
+      }
+    })
+    .code(200)
+}
+
+function updateBook (request, h) {
+  const { bookId } = request.params
+  const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload
+  const indexBook = storedBooks.findIndex((it) => it.id === bookId)
+
+  if (indexBook === -1) {
+    return h
+      .response({
+        status: 'fail',
+        message: 'Gagal memperbarui buku. Id tidak ditemukan'
+      })
+      .code(404)
+  }
+
+  if (!name) {
+    return h
+      .response({
+        status: 'fail',
+        message: 'Gagal memperbarui buku. Mohon isi nama buku'
+      })
+      .code(400)
+  }
+
+  if (readPage > pageCount) {
+    return h
+      .response({
+        status: 'fail',
+        message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount'
+      })
+      .code(400)
+  }
+
+  const now = new Date().toISOString()
+  storedBooks[indexBook] = {
+    ...storedBooks[indexBook],
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished: pageCount === readPage,
+    reading,
+    updatedAt: now
+  }
+
+  return h
+    .response({
+      status: 'success',
+      message: 'Buku berhasil diperbarui'
+    })
+    .code(200)
+}
+
+function deleteBook (request, h) {
+  const { bookId } = request.params
+  const indexBook = storedBooks.findIndex((book) => book.id === bookId)
+
+  if (indexBook === -1) {
+    return h
+      .response({
+        status: 'fail',
+        message: 'Buku gagal dihapus. Id tidak ditemukan'
+      })
+      .code(404)
+  }
+
+  storedBooks.splice(indexBook, 1)
+  return h
+    .response({
+      status: 'success',
+      message: 'Buku berhasil dihapus'
+    })
+    .code(200)
+}
+
+module.exports = { storeBooks, getAllBooks, getDetailBook, updateBook, deleteBook }
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
-const books = require('./endpoint/books')
+const { storeBooks, getAllBooks, getDetailBook, updateBook, deleteBook } = require('./endpoint/books')
 
 const routes = [
-  {
-    method: 'POST', path: '/books', handler: books
-  }
+  { method: 'GET', path: '/books', handler: getAllBooks },
+  { method: 'GET', path: '/books/{bookId}', handler: getDetailBook },
+
+  { method: 'POST', path: '/books', handler: storeBooks },
+
+  { method: 'PUT', path: '/books/{bookId}', handler: updateBook },
+
+  { method: 'DELETE', path: '/books/{bookId}', handler: deleteBook }
 ]
 
 module.exports = routes
